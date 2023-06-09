48852eefe409826e7602571c4ffe155d8d048a5b
SkyXJW <135524222+SkyXJW@users.noreply.github.com>
2023-06-09 20:48:16 +0800

    Implement the execution of CREATE
    * xiugai-XJW
    
    实现AST与CREATE的基础结合，新增not_null_flag、primary_key、attri_len来记录表中各属性是否not null、主码 、（这里仅考虑cahr与varchar类型的）属性长度
    
    * commit 2

--- sql_control/main.py
+++ sql_control/main.py
@@ -8,7 +8,7 @@ class blabla:
         示例用
         """
         self.db = DB()
-        self.db.create("test",["索引","姓名"], [int,str])
+        self.db.create("test",["索引","姓名"], ["int","varchar"],[True,False],["索引"],["姓名"],[20])
         test_table = self.db.database['test']['tabledata']
         flag, test_table = self.db.insert(table=test_table,attributes=["索引"],values=[[23],[24]])
         flag, test_table = self.db.update(table=test_table,update_rows=None,attributes=["姓名"], values=[["张三"],["王五"]])
@@ -48,11 +48,36 @@ class blabla:
         # UPDATE (SELECT * FROM test) SET id = 1 不成立
         if "SELECT" in self.clause.keys():
             self.execute_query_statement()
+        elif "CREATE" in self.clause.keys():
+            self.excute_create_statement()
 
     def execute_query_statement(self):
         # 示例用，演示怎么从AST的解析中获取table
         tables : list[pd.DataFrame] = self.get_tables()
         print(tables)
+    
+    def excute_create_statement(self):
+        table = self.clause["CREATE"].content[0]
+        attribute_ls = []
+        types_ls = []
+        not_null_ls = []
+        primary_key_ls = []
+        char_attri_ls = []
+        char_attri_len_ls = []
+        for i in range(len(self.clause["COLUMNS"].content)):
+            attribute_ls.append(self.clause["COLUMNS"].content[i].content[0]["name"])
+            types_ls.append(self.clause["COLUMNS"].content[i].content[0]["type"])
+            if (self.clause["COLUMNS"].content[i].content[0]["type"]=="char") or (self.clause["COLUMNS"].content[i].content[0]["type"]=="varchar"):
+                 char_attri_ls.append(self.clause["COLUMNS"].content[i].content[0]["name"])
+                 char_attri_len_ls.append(self.clause["COLUMNS"].content[i].content[0]["length"])
+            not_null_ls.append(self.clause["COLUMNS"].content[i].content[0]["PRIMARY"])
+            if self.clause["COLUMNS"].content[i].content[0]["PRIMARY"]:
+                primary_key_ls.append(self.clause["COLUMNS"].content[i].content[0]["name"])
+        if self.db.create(table=table, attributes=attribute_ls, types=types_ls, not_null=not_null_ls, primary_key=primary_key_ls,char_attri=char_attri_ls,char_attri_len=char_attri_len_ls):
+            print("创建成功!")
+            print(self.db.database[table]["not_null_flag"])
+        else:
+            print("创建失败!")
 
     def get_tables(self):
         #从FROM clause中获得content
@@ -68,10 +93,23 @@ class blabla:
 
 
 if __name__ == "__main__":
+    # sql = """
+    # SELECT id, name, this
+    # FROM test
+    # WHERE id = 1 AND this < 2.3;
+    # """
+    # a = blabla()
+    # a.execute(sql)
+    #测试create
     sql = """
-    SELECT id, name, this
-    FROM test
-    WHERE id = 1 AND this < 2.3;
+    CREATE TABLE Persons
+    (
+        PersonID SERIAL int,
+        LastName PRIMARY varchar(255),
+        FirstName char(255) NOT NULL,
+        Address float,
+        City varchar(255)
+    );
     """
     a = blabla()
     a.execute(sql)
